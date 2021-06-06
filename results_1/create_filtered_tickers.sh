6fa32bf36ae5a361cf517b5b958bbfd4f07e896f
Jyzaya <JyzayaEU@gmail.com>
2021-06-06 19:07:54 +0200

    Update results to answer q4
    The previous results were wring due to a bug in one of the
    dependencies of the code and also missing a closer analysis of
    the data.
    
    This commit contains a full analysis document together with
    aggregated data.

--- results_1/create_filtered_tickers.sh
+++ results_1/create_filtered_tickers.sh
@@ -1,7 +1,20 @@
+set -eo pipefail
+
+# Store dir of this script since we want to put reports into it
+BASEDIR=$(dirname "$0")
+TICKER_COUNTS_UNFITLERED_FILE=$BASEDIR/unfiltered_ticker_counts.csv
+echo "Replacing ticker file which stores the original unfiltered ticker counts: $TICKER_COUNTS_UNFITLERED_FILE"
+echo "Exchange,Min Marketcap,Ticket Count" > $TICKER_COUNTS_UNFITLERED_FILE
+
 for EXCHANGE in nyse nasdaq amex
 do
     for MARKETCAP in 1000 100 10
     do
+        # Update the unfiltered ticker count file based on the source repos
+        SOURCE_DIR=./ticker_data__"$EXCHANGE"_min_"$MARKETCAP"m
+        TICKER_COUNT=`ls $SOURCE_DIR | wc -l | tr -d ' '`
+        echo "$EXCHANGE,$MARKETCAP,$TICKER_COUNT" >> $TICKER_COUNTS_UNFITLERED_FILE
+
         for MULTIPLIER in 2 3 5
         do
             for DAYS in 5 10
@@ -11,18 +24,14 @@ do
                 IDENTIFIER="$EXCHANGE"_min_"$MARKETCAP"_multi_"$MULTIPLIER"_days_"$DAYS"
                 echo "Current step: $IDENTIFIER"
 
-                SOURCE_DIR=./ticker_data__"$EXCHANGE"_min_"$MARKETCAP"m
-                echo "Source contains `ls $SOURCE_DIR | wc -l` tickers."
-
                 OUTPUT_DIR=./ticker_data__"$IDENTIFIER"
-                mkdir $OUTPUT_DIR
+                mkdir -p $OUTPUT_DIR
                 poetry run python bin/pull_data.py -v --$EXCHANGE --min-market-cap=$MARKETCAP \
                   --ticker-source-dir $SOURCE_DIR --output-path $OUTPUT_DIR \
                   --filters q4_majorshortsqueezes.filter/price_multi_"$MULTIPLIER"_within_"$DAYS"_days >"$IDENTIFIER".log 2>&1
                 # Create summaries/ ticker lists from log data
-                BASEDIR=$(dirname "$0")
-                grep -Eo '{.*}' "$IDENTIFIER".log | poetry run python $BASEDIR/transform_ljson_to_csv.py > $BASEDIR/"$IDENTIFIER".csv
+                grep INFO "$IDENTIFIER".log | grep -Eo '{"Ticker":.*}' | poetry run python $BASEDIR/transform_ljson_to_csv.py > $BASEDIR/"$IDENTIFIER".csv
             done
-        done        
+        done
     done
 done
